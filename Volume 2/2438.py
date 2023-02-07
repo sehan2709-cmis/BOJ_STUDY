c45a4abe1861d30a12dac1fa7a59448e58f7768e
Sehan Kwon <42161023+sehan2709-cmis@users.noreply.github.com>
2023-02-07 23:55:52 +0900

    2438, 2557, 2562 bronze questions solved in few min

---
+++ Volume 2/2438.py
@@ -0,0 +1,4 @@
+input = int(input())
+
+for i in range(1, input+1):
+  print("*" * i)
---
+++ Volume 2/2562.py
@@ -0,0 +1,6 @@
+numbers = []
+for i in range(9):
+    numbers.append(int(input()))
+    
+print(max(numbers))
+print(numbers.index(max(numbers))+1)
