dd607c11a61ac449d869a8da50fd264a919112a3
Sehan Kwon <sehan2709@cmis.ac.th>
2025-05-12 22:43:30 +0900

    10797

--- ~2025/gold/1956.py
+++ ~2025/gold/1956.py
@@ -1,28 +1,50 @@
-#input = open('input.txt','r').readline #개인 확인용
-
-
+import sys
+input = sys.stdin.readline
 
 V, E = map(int, input().split())
+INF = float('inf')
 
-graph = [[float('inf') for _ in range(V)] for _ in range(V)]
+# 그래프 초기화
+matrix = [[INF for _ in range(V)] for _ in range(V)]
+for i in range(V):
+    matrix[i][i] = 0
 
-print(graph)
-#for i in range(E):
-#   print("a")
-    
+# 간선 입력
+for _ in range(E):
+    a, b, cost = map(int, input().split(' '))
+    matrix[a-1][b-1] = cost  # 1-based index를 0-based index로 변환
+
+# 플로이드 워셜 알고리즘
+for k in range(V):
+    for i in range(V):
+        for j in range(V):
+            matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j])
+
+# 최소 사이클 계산
+minCycle = float('inf')
+for i in range(V):
+    minCycle = min(minCycle, matrix[i][i])
+
+# 결과 출력
+if minCycle == float('inf'):
+    print(-1)
+else:
+    print(minCycle)
+
+
 '''
 백준 1956 운동
 
 입력
-첫째 줄에 V와 E가 빈칸을 사이에 두고 주어진다. (2 ≤ V ≤ 400, 0 ≤ E ≤ V(V-1))
-다음 E개의 줄에는 각각 세 개의 정수 a, b, c가 주어진다.
-
+첫째 줄에 V와 E가 빈칸을 사이에 두고 주어진다.
+(2 ≤ V ≤ 400, 0 ≤ E ≤ V(V-1)) 다음 E개의 줄에는 각각 세 개의 정수 a, b, c가 주어진다.
 a번 마을에서 b번 마을로 가는 거리가 c인 도로가 있다는 의미이다.
 (a → b임에 주의) 거리는 10,000 이하의 자연수이다.
 (a, b) 쌍이 같은 도로가 여러 번 주어지지 않는다.
 
 출력
-첫째 줄에 최소 사이클의 도로 길이의 합을 출력한다. 운동 경로를 찾는 것이 불가능한 경우에는 -1을 출력한다.
+첫째 줄에 최소 사이클의 도로 길이의 합을 출력한다.
+운동 경로를 찾는 것이 불가능한 경우에는 -1을 출력한다.
 
 예제 입력:
 3 4
